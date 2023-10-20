349107a729fc3fc4b4bab402b2c23c0f9d693a59
emma4700 <139786999+emma4700@users.noreply.github.com>
2023-10-20 08:32:54 +0600

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,1 @@
-const offerLink = "https://tinyurl.com/Email4700";
+const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqbWJsazcyanhQUTNUMTJlV0IyaXUzT0VlbUpRUXxBQ3Jtc0tsdnptamJfSVVVSzJwU01wSU10ekhaZTEycGZzZDVMdG1vYUF3cXhPajVvSmtzS09SbzNRckVJRnlJVkxORjkxUko1QVBrdmZVdGpLNy1ZcVdRcHRSNThrWnQwY04xZFZzVmdNRnlTY3JfOVM5QTNqOA&q=https%3A%2F%2Ftinyurl.com%2F2j2wfry7&html_redirect=1";
